096e0943f455aa2a0a6607e05788f7cf0a7f1725
michaelmburu <ichangaimichael@yahoo.com>
2021-07-25 13:24:48 +0300

    Add project files.

---
+++ HttpFunctions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoProcessor
+{
+    public static class HttpFunctions
+    {
+        [FunctionName("Function1_HttpStart")]
+        public static async Task<IActionResult> HttpStart(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+          [DurableClient] IDurableOrchestrationClient starter,
+          ILogger log)
+        {
+            string video = req.GetQueryParameterDictionary()["video"];
+            if(video == null)
+            {
+                return new BadRequestObjectResult("Please pass the video location of the query string");
+            }
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("ProcessVideoOrchestrator", null, video);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
---
+++ OrchestratorFunctions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoProcessor
+{
+    public static class OrchestratorFunctions
+    {
+        [FunctionName(nameof(ProcessVideoOrchestrator))]
+        public static async Task<object> ProcessVideoOrchestrator(
+         [OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
+        {
+            log = context.CreateReplaySafeLogger(log);
+            var videoLocation = context.GetInput<string>();
+
+            log.LogInformation("about to call transcode video activity");
+            var transcodedLocation = await context.CallActivityAsync<string>("TranscodeVideo", videoLocation);
+            log.LogInformation("about to call extract thumbnail activity");
+            var thumbnalLocation = await context.CallActivityAsync<string>("ExtractThumbnail", transcodedLocation);
+            log.LogInformation("about to call prepepnd intro activty");
+            var withIntroLocation = await context.CallActivityAsync<string>("PrependIntro", transcodedLocation);
+
+            return new
+            {
+                Transcoded = transcodedLocation,
+                Thumbnail = thumbnalLocation,
+                WithIntro = withIntroLocation
+            };
+
+        }
+    }
+}
